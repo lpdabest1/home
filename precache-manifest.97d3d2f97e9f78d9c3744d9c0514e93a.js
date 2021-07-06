61796f6c21640d8a0f4e83d8c5589744127c50a8
lpdabest1 <lorenzo.pesina@utexas.edu>
2021-07-05 19:47:05 -0500

    Updates

---
+++ precache-manifest.97d3d2f97e9f78d9c3744d9c0514e93a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95b942e70901c67caf43a47883471592",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83c1a9770f82424356e5",
+    "url": "/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "ae3bc072d95b50882689",
+    "url": "/static/js/2.27abb272.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.27abb272.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "83c1a9770f82424356e5",
+    "url": "/static/js/main.3eb9ca67.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "bc004cd188255743ed67fa59fda602ea",
+    "url": "/static/media/IFIXIT Page.bc004cd1.PNG"
+  },
+  {
+    "revision": "5bcb7dfa1d70b4d4cecfd494a43e2157",
+    "url": "/static/media/RenzoeBox Website.5bcb7dfa.PNG"
+  },
+  {
+    "revision": "429d9c1eda9d4205c9fec8d585ebc61f",
+    "url": "/static/media/Tech Doc.429d9c1e.PNG"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  },
+  {
+    "revision": "5a4283108df1aed97bcbc980fee3c810",
+    "url": "/static/media/utsystem.5a428310.png"
+  }
+]);
